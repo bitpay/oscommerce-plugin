e77769858b41c66ab5f4fe2919f6bca8f60242d6
Rich Morgan <ionux@users.noreply.github.com>
2014-01-28 14:42:45 -0500

    Updated with license, invalid, & paid statuses.
    Per official BitPay API docs: A BitPay.com invoice can be in one of the following states: “new”, “paid”, “confirmed”, “complete”, “expired” or
    “invalid”.

--- bitpay_callback.php
+++ bitpay_callback.php
@@ -1,25 +1,48 @@
 <?php
+
+/**
+ * ©2011,2012,2013,2014 BIT-PAY LLC.
+ * 
+ * Permission is hereby granted to any person obtaining a copy of this software
+ * and associated documentation for use and/or modification in association with
+ * the bitpay.com service.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * Bitcoin oscommerce payment plugin using the bitpay.com service.
+ * 
+ */
+
 require 'bitpay/bp_lib.php';
 require 'includes/application_top.php';
 
 $response = bpVerifyNotification(MODULE_PAYMENT_BITPAY_APIKEY);
 
 if (is_string($response)) {
-  bpLog( 'bitpay callback error: $response');
-}
-else {
-  bpLog( 'bitpay callback: $response');
+  bpLog('bitpay callback error: $response');
+} else {
+  bpLog('bitpay callback: $response');
   $order_id = $response['posData'];
-  switch($response['status'])
-  {
+  switch($response['status']) {
+    case 'paid':
     case 'confirmed':
     case 'complete':
-	  tep_db_query("update " . TABLE_ORDERS . " set orders_status = " . MODULE_PAYMENT_BITPAY_PAID_STATUS_ID . " where orders_id = " . intval($order_id));
-	  break;
-	case 'expired':
-	  tep_remove_order($order_id, $restock = true);
-	  break;
-	}
+    	if(function_exists('tep_db_query'))
+          tep_db_query("update " . TABLE_ORDERS . " set orders_status = " . MODULE_PAYMENT_BITPAY_PAID_STATUS_ID . " where orders_id = " . intval($order_id));
+    	break;
+    case 'invalid':
+    case 'expired':
+    	if(function_exists('tep_remove_order'))
+          tep_remove_order($order_id, $restock = true);
+    	break;
+    case 'new':
+    	break;
+  }
 }
 ?>
-
